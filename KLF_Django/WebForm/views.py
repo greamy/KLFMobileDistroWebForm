e1cddfd88e951bc3179efb8494849b0f7fc05a70
greamy <grant@abgames.com>
2024-02-06 11:45:21 -0500

    Cleanup views.py

--- KLF_Django/WebForm/views.py
+++ KLF_Django/WebForm/views.py
@@ -7,20 +7,19 @@ from .Scripts.JsonHandler import JsonHandler
 from .Scripts.QRCode import QRCode
 from .models import Location, Site
 
+# create functions, urls,py calls these functions to handle urls like /admin, /about, etc
+# this will generate responses from these functions, for instance, calling the html file for the user form.
 
 def index(request):
 	return render(request, "WebForm/index.html", {})
 
-
 def admin(request):
 	return render(request, "WebForm/admin.html", {})
-
 
 def generate_QR(request):
 	QR = QRCode("google.com")
 	QR.saveImage("WebForm/QR_Codes/QR.png")
 	return HttpResponse("test")
-
 
 def submit(request):
 	if request.method == "POST":
@@ -41,13 +40,7 @@ def submit(request):
 	else:
 		return HttpResponse("Howd you do dat?")
 
-
-# create functions, urls,py calls these functions to handle urls like /admin, /about, etc
-# this will generate responses from these functions, for instance, calling the html file for the user form.
-
 def get_locations(request):
-	file_path = 'WebForm/locations.json'
-	
 	sites = Site.objects.all()
 	site_dict = {}
 	for site in sites:
@@ -56,23 +49,11 @@ def get_locations(request):
 		else:
 			site_dict[site.location.name] = [site.name]
 
-	print(site_dict)
-
-
-	try:
-		handler = JsonHandler(file_path)
-		locations = handler.get_data()
-
-		return JsonResponse(site_dict, safe=False)
-	except FileNotFoundError:
-		return JsonResponse({'error': 'Locations JSON file not found'}, status=404)
-
+	return JsonResponse(site_dict, safe=False)
 
 def create_site(request):
-	#file_path = 'WebForm/locations.json'
 	loc_name = request.POST.get("newLocation")
 	site_name = request.POST.get("newSite")
-	loc_dict = {loc_name: [site_name]}
 
 	loc = Location.objects.filter(name__iexact=loc_name)
 
@@ -85,17 +66,9 @@ def create_site(request):
 		site = Site(name=site_name, location=loc)
 		site.save()
 
-	try:
-		#handler = JsonHandler(file_path)
-		#handler.add_data(loc_dict)
-		#handler.save_json(file_path)
-		return get_locations(request)
-	except FileNotFoundError:
-		return JsonResponse({'error': 'Locations JSON file not found'}, status=404)
-
+	return get_locations(request)
 
 def delete_site(request):
-	#file_path = 'WebForm/locations.json'
 	loc_name = request.POST.get("location")
 	site_name = request.POST.get("site")
 
@@ -105,17 +78,10 @@ def delete_site(request):
 		other_sites = Site.objects.filter(location__name__iexact=loc_name)
 		if not other_sites.exists():
 			Location.objects.filter(name__iexact=loc_name).delete()
-
 	else:
 		return JsonResponse({'error': 'Site object not found in database'}, status=404)
 
-	try:
-		#handler = JsonHandler(file_path)
-		#handler.remove_data(loc_name, site_name)
-		#handler.save_json(file_path)
-		return get_locations(request)
-	except FileNotFoundError:
-		return JsonResponse({'error': 'Locations JSON file not found'}, status=404)
+	return get_locations(request)
 
 
 
