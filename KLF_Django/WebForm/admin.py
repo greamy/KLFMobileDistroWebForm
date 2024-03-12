bc29eb9f9f4cd10d07226012d359f1a4b17f9b3c
greamy <grant@abgames.com>
2024-03-12 12:44:24 -0400

    Allow Submission table date column to be viewed from Django admin page

--- KLF_Django/WebForm/admin.py
+++ KLF_Django/WebForm/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Location, Site, Submission
 
+class SubmissionAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
+
+
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Site)
-admin.site.register(Submission)
+admin.site.register(Submission, SubmissionAdmin)
