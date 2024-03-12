1ab1d3c5c39f13bdf190a40ad56e55421eb98675
greamy <grant@abgames.com>
2024-03-12 10:25:04 -0400

    Add Submission model to djangoadmin page

--- KLF_Django/WebForm/admin.py
+++ KLF_Django/WebForm/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Location, Site
+from .models import Location, Site, Submission
 
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Site)
+admin.site.register(Submission)
