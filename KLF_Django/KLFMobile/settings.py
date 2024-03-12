3808d47fae5afa538abbb1c8b4e39ea2b7336f83
greamy <grant@abgames.com>
2024-03-12 11:15:31 -0400

    Add Edit form UI to admin page

--- KLF_Django/KLFMobile/settings.py
+++ KLF_Django/KLFMobile/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-tbeq7uap#+$g#i2*gx!nw5vh=+1+kh#f1+bs$ncnv27&i*t-w6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.40.159']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.40.159', '35.46.71.79', '10.80.146.22']
 
 
 # Application definition
--- KLF_Django/WebForm/static/WebForm/admin.js
+++ KLF_Django/WebForm/static/WebForm/admin.js
@@ -12,6 +12,220 @@ $(document).ready(function () {
 	});
 });
 
+//2d Array for form
+//format: id, placeholder, name, type, required?, min, max, hide?, TEFAP
+// TODO: Replace this definition with server call on 'Edit form' button click.
+const inputSettings = [
+                       ["First Name","First Name","First Name","text", 1,0,0,0,1],
+                       ["Last Name","Last Name","Last Name","text", 1,0,0,0,1],
+                       ["Email Address","Email Address","Email Address","email",0,0,0,0,1],
+                       ["Number in Household","Number in Household","Number in Household","number",1,1,50,0,1],
+                       ["Street Address","Street Address","Street Address","other",1,0,0,0,1],
+                       ["Zip Code","Zip Code","Zip Code","number",1,10000,99999,0,1]];
+let Universal = "";
+
+//form editor
+function FormSetting(settings, x){
+  const info = document.createElement("b");
+  info.appendChild(document.createTextNode("\u24d8"));
+
+  const TEFAP = document.createElement("i");
+  TEFAP.setAttribute("title", "TEFAP");
+  TEFAP.setAttribute("class","fa fa-book");
+
+  const name = document.createElement("p");
+  if(settings[8] == 1){
+    name.appendChild(TEFAP);
+    name.appendChild(document.createTextNode(" "));
+  }
+  name.appendChild(document.createTextNode(settings[2]));
+
+  const Plabel = document.createElement("label");
+  Plabel.setAttribute("for",settings[0]);
+  Plabel.appendChild(document.createTextNode("Placeholder: "));
+  Plabel.setAttribute("style","font-size: 16px; margin-left: 10px");
+
+  const rename = document.createElement("input");
+  rename.setAttribute("placeholder",settings[1]);
+  rename.setAttribute("name",settings[2]);
+  rename.setAttribute("id",settings[0]);
+  rename.setAttribute("class","rename");
+
+  const rlabel = document.createElement("label");
+  rlabel.setAttribute("for", "require");
+  rlabel.appendChild(document.createTextNode("Required?"));
+  rlabel.setAttribute("style","font-size: 16px; margin-left: 10px");
+
+  const require = document.createElement("input");
+  require.setAttribute("type","checkbox");
+  require.setAttribute("id","require");
+  if (settings[4] == 1){
+    require.setAttribute("checked", 1);
+  }
+  const tlabel = document.createElement("label");
+  tlabel.appendChild(document.createTextNode("Type:"));
+  tlabel.setAttribute("for", "type");
+  tlabel.setAttribute("style","font-size: 16px; margin-left: 10px");
+
+  const text = document.createElement("option");
+  text.appendChild(document.createTextNode("Text"));
+  text.setAttribute("title","Accepts only Letters");
+  text.setAttribute("value","text");
+  if (settings[3] == "text"){
+    text.setAttribute("selected",1);
+  }
+
+  const Email = document.createElement("option");
+  Email.appendChild(document.createTextNode("Email"));
+  Email.setAttribute("title","Accepts only an Email Address");
+  Email.setAttribute("value","email");
+  if (settings[3] == "email"){
+    Email.setAttribute("selected",1);
+  }
+
+  const number = document.createElement("option");
+  number.appendChild(document.createTextNode("Number"));
+  number.setAttribute("title","Accepts only Numbers");
+  number.setAttribute("value","number");
+  if (settings[3] == "number"){
+    number.setAttribute("selected",1);
+  }
+
+  const other = document.createElement("option");
+  other.appendChild(document.createTextNode("Other"));
+  other.setAttribute("title","Accepts all Characters");
+  other.setAttribute("value","other");
+  if (settings[3] == "other"){
+    other.setAttribute("selected",1);
+  }
+
+  const type = document.createElement("select");
+  type.setAttribute("id","type");
+  type.appendChild(text);
+  type.appendChild(Email);
+  type.appendChild(number);
+  type.appendChild(other);
+
+  const mlabel = document.createElement("label");
+  mlabel.setAttribute("for", "min");
+  mlabel.appendChild(document.createTextNode("Min:"));
+  mlabel.setAttribute("style","font-size: 16px; margin-left: 10px");
+
+  const min = document.createElement("input");
+  min.setAttribute("id","min");
+  min.setAttribute("type","number");
+  min.setAttribute("min","0");
+  min.setAttribute("style","width:50px");
+  min.setAttribute("placeholder", settings[5]);
+
+  const xlabel = document.createElement("label");
+  xlabel.setAttribute("for", "max");
+  xlabel.appendChild(document.createTextNode("Max:"));
+  xlabel.appendChild(info);
+  xlabel.setAttribute("title","Set to 0 if you don't want a limit");
+  xlabel.setAttribute("style","font-size: 16px; margin-left: 10px");
+
+  const max = document.createElement("input");
+  max.setAttribute("id","max");
+  max.setAttribute("type","number");
+  max.setAttribute("min","0");
+  max.setAttribute("style","width:50px");
+  max.setAttribute("placeholder", settings[6]);
+
+    const upA = document.createElement("i");
+  upA.setAttribute("class","arrow up");
+
+  const up = document.createElement("button");
+  up.setAttribute("class","arrows");
+  up.setAttribute("id","up");
+  up.appendChild(upA);
+
+  const downA = document.createElement("i");
+  downA.setAttribute("class","arrow down");
+
+  const down = document.createElement("button");
+  down.setAttribute("class","arrows");
+  down.setAttribute("id","down");
+  down.appendChild(downA);
+
+  const nOrder = document.createElement("div");
+  nOrder.setAttribute("class","nOrder");
+  nOrder.appendChild(document.createTextNode(x+1));
+
+
+  const order = document.createElement("div");
+  order.setAttribute("id","orderStyle");
+  order.appendChild(up);
+  order.appendChild(nOrder);
+  order.appendChild(down);
+
+  const LRemove = document.createElement("i");
+  LRemove.appendChild(document.createTextNode("X"));
+
+  const Remove = document.createElement("button");
+  Remove.setAttribute("id","Remove");
+  if (settings[8]==1){
+    Remove.setAttribute("Disabled",1);
+  }
+  Remove.appendChild(LRemove);
+
+  const visible = document.createElement("i");
+  visible.setAttribute("class","fa fa-eye");
+  visible.setAttribute("title","Visibility");
+
+  const Vcheck = document.createElement("input");
+  Vcheck.setAttribute("type","checkbox");
+  if (settings[7]==0){
+    Vcheck.setAttribute("checked", 1);
+  }
+
+  const Visdiv = document.createElement("div");
+  Visdiv.appendChild(visible);
+  Visdiv.appendChild(Vcheck);
+  Visdiv.setAttribute("class","Visdiv");
+
+  const option = document.createElement("div");
+  option.setAttribute("id","option");
+  option.appendChild(name);
+  option.appendChild(Plabel);
+  option.appendChild(rename);
+  option.appendChild(rlabel);
+  option.appendChild(require);
+  option.appendChild(tlabel);
+  option.appendChild(type);
+  if (settings[3] == "number"){
+    option.appendChild(mlabel);
+    option.appendChild(min);
+    option.appendChild(xlabel);
+    option.appendChild(max);
+  }
+  option.appendChild(order);
+  option.appendChild(Remove);
+  option.appendChild(Visdiv);
+
+  const optionView = document.createElement("div");
+  optionView.appendChild(option);
+
+  const Save = document.createElement("button");
+  Save.setAttribute("id","SaveForm");
+  Save.appendChild(document.createTextNode("Save Form"));
+
+  const add = document.createElement("button");
+  add.setAttribute("id","AddField");
+  add.appendChild(document.createTextNode("Add Field"));
+
+  const FormButtons = document.createElement("div");
+  FormButtons.setAttribute("class","FormButton");
+  FormButtons.appendChild(Save);
+  FormButtons.appendChild(add);
+
+  const formSetting = document.getElementById("form-setting");
+  formSetting.appendChild(optionView);
+  if(x+1 == inputSettings.length){
+    formSetting.appendChild(FormButtons);
+  }
+}
+
 function processData(data) {
 	var rows = data;
 	var locations = [];
@@ -41,7 +255,7 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-let Universal = ""; // do not touch
+
 function createSite() {
 	const loc = document.getElementById("InputLocation");
 	const site = document.getElementById("InputSite");
@@ -296,4 +510,7 @@ function populateLocations(locations, sites) {
 			Cpage = Npage;
 		});
 	}
+}
+for (var i = 0; i < inputSettings.length;i++){
+  		FormSetting(inputSettings[i], i);
 }
