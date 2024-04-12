bf5987006a961e9b6f83b0d781c041dbccda569c
greamy <grant@abgames.com>
2024-04-12 16:30:27 -0400

    Allow username or email to be used for forgot password

--- KLF_Django/WebForm/views.py
+++ KLF_Django/WebForm/views.py
@@ -96,29 +96,32 @@ def change_username(request):
 	new_username = request.POST["username"]
 	new_email = request.POST['email']
 
-	if request.user.email != new_email:
-		send_mail(
-			subject="Alert: KL&F Mobile Distribution System Email Change",
-			message="The email used for password recovery of the admin account has been changed.\n" +
-					"The new email for the admin account is: " + new_email + "\n" +
-					"If you were not aware of this change, change your password immediately.\n" +
-					"This can be done via the 'edit profile' dropdown in the top right of the admin panel.",
-			from_email=None,
-			recipient_list=[request.user.email, new_email],
-			fail_silently=True
-		)
+	try:
+		if request.user.email != new_email:
+			send_mail(
+				subject="Alert: KL&F Mobile Distribution System Email Change",
+				message="The email used for password recovery of the admin account has been changed.\n" +
+						"The new email for the admin account is: " + new_email + "\n" +
+						"If you were not aware of this change, change your password immediately.\n" +
+						"This can be done via the 'edit profile' dropdown in the top right of the admin panel.",
+				from_email=None,
+				recipient_list=[request.user.email, new_email],
+				fail_silently=True
+			)
 
-	if request.user.username != new_username:
-		send_mail(
-			subject="Alert: KL&F Mobile Distribution System Username Change",
-			message="The username used for the admin account login has been changed.\n" +
-					"The new username for the admin account is: " + new_username + "\n" +
-					"If you were not aware of this change, please change your password immediately.\n" +
-					"This can be done via the 'edit profile' dropdown in the top right of the admin panel.",
-			from_email=None,
-			recipient_list=[new_email],
-			fail_silently=True
-		)
+		if request.user.username != new_username:
+			send_mail(
+				subject="Alert: KL&F Mobile Distribution System Username Change",
+				message="The username used for the admin account login has been changed.\n" +
+						"The new username for the admin account is: " + new_username + "\n" +
+						"If you were not aware of this change, please change your password immediately.\n" +
+						"This can be done via the 'edit profile' dropdown in the top right of the admin panel.",
+				from_email=None,
+				recipient_list=[new_email],
+				fail_silently=True
+			)
+	except ValueError:
+		pass
 
 	request.user.username = new_username
 	request.user.email = new_email
@@ -175,24 +178,29 @@ def forgot_password(request):
 	if request.method == "GET":
 		return render(request, "WebForm/ForgotPassword.html", {})
 	if request.method == "POST":
-		email = request.POST['Email']
+		# User can input either email or username associated with the admin account.
+		email_username = request.POST['EmailUsername']
 		admin_users = User.objects.all()
 		for user in admin_users:
-			if user.email == email:
+			if user.email == email_username or user.username == email_username:
 				code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
 				print(code)
-				# TODO: save code to database for later use
-				new_code = PasswordCode(code=code, email=email)
+
+				new_code = PasswordCode(code=code, email=email_username)
 				new_code.save()
-				send_mail(
-					subject="Forgot Password Request KL&F Mobile Distribution System",
-					message="A forgot password request for the admin page was sent.\n" +
-							"The code to reset your password is: " + code + ".\n" +
-							"If you did not request this code, disregard this email.",
-					from_email=None,
-					recipient_list=[email],
-					fail_silently=True
-				)
+				try:
+					send_mail(
+						subject="Forgot Password Request KL&F Mobile Distribution System",
+						message="A forgot password request for the admin page was sent.\n" +
+								"The code to reset your password is: " + code + ".\n" +
+								"If you did not request this code, disregard this email.",
+						from_email=None,
+						recipient_list=[user.email],
+						fail_silently=True
+					)
+				except ValueError:
+					return render(request, "WebForm/ForgotPassword.html", {"error": "Email client not set up. " +
+																					"Forgot password functionality disabled."})
 		return render(request, "WebForm/ForgotPasswordCode.html", {})
 	else:
 		return HttpResponseBadRequest(INVALID_REQUEST_TYPE)
@@ -208,7 +216,7 @@ def forgot_password_code(request):
 	print(timeout)
 	codes_db = PasswordCode.objects.filter(generation_time__gte=timeout)
 	if codes_db.exists():
-		code_db = codes_db.first()
+		code_db = codes_db.last()
 		if code_submitted == code_db.code:
 			return render(request, "WebForm/ForgotPasswordChange.html", {"email": code_db.email})
 
